45a2c378db501ceaf609ded8d9c383622e9383d3
codemodel6 <codemodel6@gmail.com>
2024-10-15 14:14:25 +0900

    desgin : 메인페이지 디자인 완성

---
+++ src/app/board[_id]/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>안녕하세요</div>;
+};
+
+export default page;
--- src/components/molecule/carousel/home-carousel/HomeCarousel.tsx
+++ src/components/molecule/carousel/home-carousel/HomeCarousel.tsx
@@ -3,7 +3,8 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
 import Image from "next/image";
-import testImg from "@/assets/img/homeImg/test.png";
+import testImg from "@/assets/img/homeImg/test.jpg";
+import testImg2 from "@/assets/img/homeImg/test2.jpg";
 
 const HomeCarousel = () => {
   const settings = {
@@ -12,9 +13,9 @@ const HomeCarousel = () => {
     slidesToScroll: 1,
     infinite: true,
     arrows: false,
-    autoplay: false,
-    speed: 1500,
-    autoplaySpeed: 3000,
+    autoplay: true,
+    speed: 1000,
+    autoplaySpeed: 5000,
     cssEase: "linear",
     pauseOnHover: false, // 마우스 호버에도 자동으로 넘어감
   };
@@ -24,11 +25,36 @@ const HomeCarousel = () => {
       <div className="carousel">
         <Slider {...settings}>
           <div className={styles["img-wrapper"]}>
-            <Image src={testImg} alt="테스트" />
+            <div className={styles["overlay"]}>
+              <div className={styles["text-block"]}>
+                <h3>깨달음</h3>
+                <span>누구나 쉽게 접근 가능한 배움을 얻을 수 있는 곳</span>
+              </div>
+            </div>
+            <Image
+              src={testImg}
+              alt="테스트"
+              layout="fill"
+              objectFit="cover"
+              objectPosition="center"
+              priority
+            />
           </div>
-          <div className={styles["text-wrapper"]}>
-            <div className={styles["title"]}>접근</div>
-            <div className={styles["contents"]}>테스트</div>
+          <div className={styles["img-wrapper"]}>
+            <div className={styles["overlay"]}>
+              <div className={styles["text-block"]}>
+                <h3>쉬운 접근</h3>
+                <span>가장 빠르고 가장 쉬운 접근</span>
+              </div>
+            </div>
+            <Image
+              src={testImg2}
+              alt="테스트2"
+              layout="fill"
+              objectFit="cover"
+              objectPosition="center"
+              priority
+            />
           </div>
         </Slider>
       </div>
